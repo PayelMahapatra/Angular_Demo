95cbc774f5e16416a0d057086f8e158f2b502623
PayelMahapatra <payelmahapatra93@gmail.com>
2019-05-17 08:14:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9c4iqe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9c4iqe)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { User } from './address-card/user.model';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+
+  user : User;
+  constructor(){
+    this.user = new User();
+    this.user.name = 'Payel Mahapatra';
+    this.user.title = 'Software Developer';
+    this.user.address = 'Kolkata';
+    this.user.phone = ['123-456-789', '852-741-963'];
+  }
+}
